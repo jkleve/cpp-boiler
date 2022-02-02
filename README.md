91e92a7c26dc13818104fd8137b294c592636add
Jesse Kleve <jesse.kleve@gmail.com>
2022-02-01 21:34:14 -0700

    Add install

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cpp-boiler
 C++17 boiler
+
+## Install
+```sh
+bash <(curl -fsSL https://raw.githubusercontent.com/jkleve/cpp-boiler/main/install.sh)
+```
---
+++ install.sh
@@ -0,0 +1,10 @@
+
+main() {
+    git clone https://github.com/jkleve/cpp-boiler.git
+    cd cpp-boiler
+
+    # remove unwanted files
+    rm -rf .git/
+}
+
+main
